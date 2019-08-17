e76d034e4b7337d8fb6588be008631880af4ebe0
Crazykulou <wxwx5566@163.com>
2019-08-17 20:22:34 +0800

    init project

---
+++ easy-document-manage/src/main/java/com/crazykulou/easy/document/config/datasource/PluginsConfig.java
@@ -0,0 +1,44 @@
+package com.crazykulou.easy.document.config.datasource;
+
+import cn.stylefeng.roses.core.metadata.CustomMetaObjectHandler;
+import com.crazykulou.easy.document.sys.core.shiro.ShiroKit;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mp的插件拓展和资源扫描
+ *
+ * @author fengshuonan
+ * @Date 2019/5/10 21:33
+ */
+@Configuration
+@MapperScan(basePackages = {
+        "com.crazykulou.easy.document.sys.modular.*.mapper",
+        "com.crazykulou.easy.document.manage.modular.*.mapper"})
+public class PluginsConfig {
+
+    /**
+     * 拓展核心包中的字段包装器
+     *
+     * @author fengshuonan
+     * @Date 2019/5/10 21:35
+     */
+    @Bean
+    public CustomMetaObjectHandler gunsMpFieldHandler() {
+        return new CustomMetaObjectHandler() {
+
+            @Override
+            protected Long getUserUniqueId() {
+                try {
+                    return ShiroKit.getUserNotNull().getId();
+                } catch (Exception e) {
+
+                    //如果获取不到当前用户就存空id
+                    return -100L;
+                }
+            }
+        };
+    }
+
+}
---
+++ easy-document-manage/src/main/java/com/crazykulou/easy/document/core/package-info.java
@@ -0,0 +1,1 @@
+package com.crazykulou.easy.document.core;
